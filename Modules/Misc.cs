a361c3bf6ebf005710706bdb39eb49b1de1a6484
Sebastian Porsche <sebastian@birdie.codes>
2019-02-27 12:20:15 +0100

    Changed up the command structure

--- Modules/Misc.cs
+++ Modules/Misc.cs
@@ -11,30 +11,31 @@ namespace NewTestBot.Modules
 
     public class Misc : ModuleBase<SocketCommandContext>
     {
+//---------------------------------------------------------------------------------------------------------
+//command works fine
+
         [Command("bab")]
-        public async Task Echo([Remainder]string message)
+        public async Task Bab([Remainder]string message)
         {
             var embed = new EmbedBuilder();
-            {
-                embed.WithTitle("BABBER-BOT9000");
-                embed.WithDescription(message + " Has been bab'd by " + Context.User.Username);
-            };
-            embed.AddField("Your options are:",
-                "Go commit rope :thinking: \n BAB back :fist: ")
-                //.WithAuthor(Context.Client.CurrentUser)
-                .WithFooter(footer => footer.Text = "Babber xd")
+                embed.AddField("Your options are:",
+                "Go commit rope :thinking: \n BAB back :fist: \n Go cry in a corner :cry: ")
                 .WithColor(new Color(253, 246, 56))
                 .WithTitle("BABBER-BOT9000")
                 .WithDescription(message + " Has been bab'd by " + Context.User.Username)
                 .WithCurrentTimestamp()
+                .WithFooter(footer => {footer
+                .WithText("Birdie Zukira")
+                .WithIconUrl("https://cdn.discordapp.com/avatars/467437867065540620/4afd5b905bbcb1a186d7e55dff0c2e92.png");
+                })
                 .Build();
-            
-                
-                
-
-            await Context.Channel.SendMessageAsync("", false, embed);
+                      
+                await Context.Channel.SendMessageAsync("", false, embed);
         }
 //---------------------------------------------------------------------------------------------------------
+//kinda works, it checks the user sending the command if he has permissions but doesnt kick the target
+//console doesnt say anything when the command is issued correctly
+
         [Command("kick")]
         [RequireBotPermission(Discord.GuildPermission.KickMembers)]
         [RequireUserPermission(Discord.GuildPermission.KickMembers)]
@@ -50,6 +51,9 @@ namespace NewTestBot.Modules
             }
         }
 //---------------------------------------------------------------------------------------------------------
+//annouce message not working
+//idk why
+
         public async Task AnnounceJoinedUser(SocketGuildUser user)
         {
             var embed = new EmbedBuilder();
@@ -60,6 +64,9 @@ namespace NewTestBot.Modules
             await Context.Channel.SendMessageAsync("", false, embed);
         }
 //---------------------------------------------------------------------------------------------------------
+//command still not working
+//counter has to be saved into a JSON file for it to read and write the number
+
         [Command("kaffe")]
         public async Task Kaffe([Remainder]int x)
         {
@@ -84,17 +91,22 @@ namespace NewTestBot.Modules
             await Context.Channel.SendMessageAsync("", false, embed);
         }
 //---------------------------------------------------------------------------------------------------------
+//doesnt send the embeded image, just the link
+
         [Command("birb")]
         public async Task birb()
         {
             var url = "https://random.birb.pw/tweet/random";
             var embed = new EmbedBuilder();
             embed.AddField("Your daily dose of random birbs",
-                url)
+            url)
             .WithAuthor(Context.Client.CurrentUser)
-            .WithColor(new Color(253, 246, 56))
+            .WithColor(new Color(13, 255, 107))
             .WithTitle("Enjoy your Birb")
-            .WithFooter(footer => footer.Text = "Babber xd")
+            .WithFooter(footer => {footer
+            .WithText("Birdie Zukira")
+            .WithIconUrl("https://cdn.discordapp.com/avatars/467437867065540620/4afd5b905bbcb1a186d7e55dff0c2e92.png");
+            })
             .WithCurrentTimestamp()
             .Build();
 
