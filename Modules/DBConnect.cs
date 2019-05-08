27f1b9952e7d7c73c145da99531eead89e8b5887
Sebastian Porsche <sebastian@birdie.codes>
2019-05-08 11:28:44 +0200

    added update command and beginning on role creation

--- Modules/DBConnect.cs
+++ Modules/DBConnect.cs
@@ -21,11 +21,15 @@ namespace NewTestBot.Modules
         readonly string thumbnail = "https://i.gyazo.com/f67d7843f1e9e918fb85816ab4a34181.png";
 
         [Command("Connect", RunMode = RunMode.Async)] 
-        public async Task ConnectDB(string response)
+        public async Task ConnectDB(string response, params string[] args)
         {
+            //taking the response from the user, converts it to string and removing the .connect part
+            string userMessage = Context.Message.ToString();
+            string name = userMessage.Substring(userMessage.IndexOf(' ') + 1);
+
             //replacing space with "%20"
-            string account = response.Replace("_", "%20");
-            
+            string account = name.Replace(" ", "%20");
+            Console.WriteLine(account);
             //reading db info and apikey from file
             string data = File.ReadAllText("Resources/config.json");
             
@@ -39,27 +43,56 @@ namespace NewTestBot.Modules
             (string)o["database"]["dbhost"], (string)o["database"]["dbuser"], (string)o["database"]["dbname"], (string)o["database"]["dbport"], (string)o["database"]["dbpass"]);
 
             //getting league account ID
-            //using "i" for id
+            //using "i" and "f" for id
+            //if account doesnt exist - throw error and end
+            JObject f = null;
+            try
+            {
                 string responsename = c.DownloadString("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + account + "?api_key=" + apikey + "");
                 JObject i = JObject.Parse(responsename);
-                var id = i["id"];
-                var lolname = i["name"];
-                var icon = i["profileIconId"];
+                f = i;
+                
+            }
+            catch (Exception)
+            {
+                 var embed = new EmbedBuilder();
+                    embed.AddField("Connecting you...",
+                    "Summoner account doesnt exist!")
+                    .WithAuthor(author => { author
+                    .WithName("Birdie Bot")
+                    .WithIconUrl(IconURL);
+                    })
+                    .WithThumbnailUrl(thumbnail)
+                    .WithColor(new Color(255, 83, 13))
+                    .WithTitle("Birdie Bot nortification")
+
+                    .WithFooter(footer => { footer
+                    .WithText("Need help? Contact Birdie Zukira#3950")
+                    .WithIconUrl(IconURL);
+                    })
+                    .WithCurrentTimestamp()
+                    .Build();
+
+                    await Context.Channel.SendMessageAsync("", false, embed);
+            }
+
+            //getting id, name and icon id from response
+            var id = f["id"];
+            var lolname = f["name"];
+            var icon = f["profileIconId"];
 
             //getting league account icon
-                string findlatestlolversion = c.DownloadString("https://ddragon.leagueoflegends.com/api/versions.json");
+            string findlatestlolversion = c.DownloadString("https://ddragon.leagueoflegends.com/api/versions.json");
                 JArray latestlolversion = JArray.Parse(findlatestlolversion);
                 var version = latestlolversion[0];
                 string thumbnailURL = "http://ddragon.leagueoflegends.com/cdn/" + version + "/img/profileicon/" + icon + ".png";
-
-
-
 
             //getting league rank from ID
             //using "r" for rank
                 string responserank = c.DownloadString("https://euw1.api.riotgames.com/lol/league/v4/positions/by-summoner/" + id + "?api_key=" + apikey + "");
                 JArray r = JArray.Parse(responserank);
                 string rank = null;
+
             //using a for loop to check all the bodies of the json
             //since each queue type is in another body
                 for (int x = 0; x < r.Count; x++)
@@ -98,7 +131,6 @@ namespace NewTestBot.Modules
 
                  while (myreader.Read())
                  {
-                    Console.WriteLine("my result is: " + Result);
                  }
 
             myconn.Close();
@@ -108,7 +140,7 @@ namespace NewTestBot.Modules
             {
                     var embed = new EmbedBuilder();
                     embed.AddField("Connecting you...",
-                    "Your League Discord account already exist in the Database!")
+                    "Your League and Discord account already exist in the Database!")
                     .WithAuthor(author => { author
                     .WithName("Birdie Bot")
                     .WithIconUrl(IconURL);
@@ -210,6 +242,92 @@ namespace NewTestBot.Modules
             }
 
           
+        }
+
+//-------------------------------------------------------------------------------------------------------------------------
+        [Command("update", RunMode = RunMode.Async)]
+        public async Task UpdateAccount()
+        {
+            //getting id of sender and selecting lol id
+            string UserID = Context.User.Id.ToString();
+            string getID = "SELECT League_id FROM users_testing WHERE Discord_Id like  '%" + UserID + "%'; ";
+            string id = null;
+
+            //getting DB information
+            string data = File.ReadAllText("Resources/config.json");
+            JObject o = JObject.Parse(data);
+            string apikey = (string)o["lolapi"]["apikey"];
+            string connect = string.Format("server={0};user={1};database={2};port={3};password={4}",
+            (string)o["database"]["dbhost"], (string)o["database"]["dbuser"], (string)o["database"]["dbname"], (string)o["database"]["dbport"], (string)o["database"]["dbpass"]);
+
+            //getting the lol id from the sender of the update command
+            MySqlConnection myconn = new MySqlConnection(connect);
+            MySqlCommand command = new MySqlCommand(getID, myconn);
+            MySqlDataReader myreader;
+            myconn.Open();
+            myreader = command.ExecuteReader();
+            while (myreader.Read())
+            {
+                data = myreader.GetString(0);
+                id = data;
+            }
+            myconn.Close();
+
+            //using c for webclient connections
+            WebClient c = new WebClient();
+
+            //getting league rank from ID
+            //using "r" for rank
+            string responserank = c.DownloadString("https://euw1.api.riotgames.com/lol/league/v4/positions/by-summoner/" + id + "?api_key=" + apikey + "");
+            JArray r = JArray.Parse(responserank);
+            string rank = null;
+
+            //using a for loop to check all the bodies of the json
+            //since each queue type is in another body
+            for (int x = 0; x < r.Count; x++)
+            {
+                if (((string)r[x]["queueType"] == "RANKED_SOLO_5x5"))
+                {
+                    var tier = (string)r[x]["tier"];
+                    var division = (string)r[x]["rank"];
+                    string soloq = tier + " " + division;
+                    rank = soloq;
+                }
+            }
+            //updating the rank of the user
+            string updaterank = "UPDATE users_testing SET `SOLO_QUEUE` = '" + rank + "' WHERE Discord_Id like  '%" + UserID + "%';";
+                MySqlCommand updatecommand = new MySqlCommand(updaterank, myconn);
+                MySqlDataReader myreader2;
+                myconn.Open();
+                myreader2 = updatecommand.ExecuteReader();
+                myconn.Close();
+
+            var embed = new EmbedBuilder();
+                    embed.AddField("updating your account...",
+                    "Your rank has been updated!")
+                    .WithAuthor(author => { author
+                    .WithName("Birdie Bot")
+                    .WithIconUrl(IconURL);
+                    })
+                    .WithThumbnailUrl(thumbnail)
+                    .WithColor(new Color(255, 83, 13))
+                    .WithTitle("Birdie Bot nortification")
+
+                    .WithFooter(footer => { footer
+                    .WithText("Need help? Contact Birdie Zukira#3950")
+                    .WithIconUrl(IconURL);
+                    })
+                    .WithCurrentTimestamp()
+                    .Build();
+
+                    await Context.Channel.SendMessageAsync("", false, embed);
+        }
+
+        //-------------------------------------------------------------------------------------------------------------------------
+        [Command("create", RunMode = RunMode.Async),RequireOwner]
+        public async Task Createroles()
+        {
+            
         }
     }
 }
