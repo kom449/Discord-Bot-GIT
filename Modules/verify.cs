4f6c10d1e80a85a8a17442fa63b54ee201231975
Søren Andreas Jensen <soer100b@videndjurs.net>
2019-05-10 13:05:09 +0200

    Verify.cs Password generator, 8 chars Upper, Lower letters and numbers Not Tested

--- Modules/verify.cs
+++ Modules/verify.cs
@@ -13,10 +13,23 @@ namespace NewTestBot.Modules
     {
         readonly string IconURL = "https://i.gyazo.com/e05bec8ae83bbd60f5ff55f48c3c30f1.png";
         readonly string thumbnail = "https://i.gyazo.com/e05bec8ae83bbd60f5ff55f48c3c30f1.png";
+        string token = "";
         [Command("verify", RunMode = RunMode.Async)]
+
+        /*
+            Generate a password
+
+            Requirements for password:
+            random 8 chars
+            letters and numbers
+            upper and lower case letters
+        */
         public async Task Verifyaccounts()
         {
-
+                const string chars = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789";
+                token = (Enumerable.Repeat(chars, 8).Select(s => s[random.Next(s.Length)]).ToArray());
+            
+            
         }
     }
 }
